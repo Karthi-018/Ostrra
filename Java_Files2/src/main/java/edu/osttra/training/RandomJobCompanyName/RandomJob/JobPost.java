ccbb061fc28aa582d08877ead1b0e23f35b3dae4
AnkitJangid <ankitjangidrps@gmail.com>
2024-07-02 11:58:15 +0530

    Temp

---
+++ Java_Files2/src/main/java/edu/osttra/training/RandomJobCompanyName/RandomJob/JobPost.java
@@ -0,0 +1,52 @@
+package edu.osttra.training.RandomJobCompanyName.RandomJob;
+
+public class JobPost {
+    long id;
+    String jobName;
+    String jobLocation;
+    String companyName;
+
+    public JobPost(long id, String jobName, String jobLocation, String companyName) {
+        this.id = id;
+        this.jobName = jobName;
+        this.jobLocation = jobLocation;
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getJobLocation() {
+        return jobLocation;
+    }
+
+    public void setJobLocation(String jobLocation) {
+        this.jobLocation = jobLocation;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: "+id+", Name: "+jobName+", Location: "+jobLocation+", Company: "+companyName;
+    }
+}
