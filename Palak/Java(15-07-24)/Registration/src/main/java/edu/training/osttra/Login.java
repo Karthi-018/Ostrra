9b7be0046399575d4edbca37af6d7c089cbe19df
Palak <128810129+palak-2701@users.noreply.github.com>
2024-07-15 16:54:56 +0530

    Add files via upload

---
+++ Palak/Java(15-07-24)/Registration/src/main/java/edu/training/osttra/Login.java
@@ -0,0 +1,52 @@
+package edu.training.osttra;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uName= request.getParameter("uname");
+		String pass= request.getParameter("pass");
+		HttpSession session=request.getSession();
+		
+		List<AddDetails> data=new ArrayList<>();
+		data.stream().filter((name)->name.uName.equals(uName)&& name.pass.equals(pass));
+	    response.getWriter().println("<h1>Hi " +uName+" to this application</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
